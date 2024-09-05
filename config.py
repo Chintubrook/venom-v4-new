969f01ed166387f3f51895f2ed9c34067c3cff36
Chintubrook <newa74925@gmail.com>
2024-09-05 15:26:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7510589975:AAH9hR6p5KLArd72jCuWezWvr3raUMNvsHY")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("CrazyXrohanXyZ")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6508557076")
 
 
 
